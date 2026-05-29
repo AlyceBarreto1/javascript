00e769ab1586a18cb0f9067ed5203f5753aa2eec
AlyceBarreto1 <alycinhapbyf@gmail.com>
2026-05-28 21:04:51 -0300

    feat: função de borda no javascript

--- script/script.js
+++ script/script.js
@@ -2,4 +2,6 @@ function agust(){
     let min = document.getElementById("suga").textContent = "SUGA";
     let yoongi = document.getElementById("suga").style.color = 'white';
     let corpo = document.getElementById("corpo").style.backgroundColor = 'black';
+    let destaque = document.querySelectorAll(".caixa");
+    destaque[0].style.border = "3px solid white";
 }
